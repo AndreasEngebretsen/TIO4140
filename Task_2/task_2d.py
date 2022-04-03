1af276ca624f7a1452e15119d6190399d71398f8
Andreas Engebretsen <andreas.engebrets1@gmail.com>
2022-04-03 19:12:58 +0200

    get in

--- Task_2/task_2d.py
+++ Task_2/task_2d.py
@@ -2,7 +2,7 @@ import numpy as np
 from math_functions import nCr
 
 
-def european_binomial_up_out(S_0, k, T, r, n, B, delta, sigma):
+def european_binomial_up_out(S_0, K, T, r, n, B, delta, sigma):
     # Length of a period
     h = T / n
 
@@ -16,40 +16,19 @@ def european_binomial_up_out(S_0, k, T, r, n, B, delta, sigma):
 
     # Calculating sum of end node values times probability for ending up in said node
     node_sum = 0
-    for i in range(n + 1):
+    for i in range(0, n + 1):
+        S_T = S_0 * u ** i * d ** (n - i)
+        value = nCr(n, i) * pu ** i * pd ** (n - i) * max(0, S_T - K)
 
-        # Probability for ending up at node with i down moves
-        prob = np.power(pu, n - i) * np.power(pd, i) * nCr(n, i)
-
-        # Value of stock at time t
-        S_T = S_0 * np.power(u, n - i) * np.power(d, i)
-
-        # Intrinsic value of call at i down moves
-        int_val = np.max([S_T - k, 0])
-
-        val = int_val * prob
-
-        # If the ending value of the stock, s_t,
-        # is above the barrier the intrinsic value of the knock-out option is zero
+        # If S_T is larger or equal to B it is knocked out and worthless
         if S_T >= B:
             p = 0
-
-        # If the ending value of the stock, s_t, is below the barrier H
-        # we have to correct for the probability of hitting it earlier
+        # If not, we want the probability that it still has value
         else:
-            # Probability of hitting the barrier before ending up at final value s_t < H
-            prob_barrier = np.exp(- 2 / (np.power(sigma, 2) * T) * abs(np.log(S_0 / B) * np.log(S_T / B)))
-
-            # Probability of not hitting the barrier, which would mean that the option stays alive and has value
-            p = 1 - prob_barrier
-
-        # Summing up after multiplying probability and intrinsic value
-        node_sum += val * p
-
-    # Discount the sum
-    option_price = np.exp(- r * T) * node_sum
-
-    return option_price
+            p = 1 - np.exp((-2/(T * (sigma**2))) * abs(np.log(S_0/B) * np.log(S_T/B)))
+        node_sum += value * p
+    node_sum = np.exp(-r * h * n) * node_sum
+    return node_sum
 
 
 def task_2d_answers():
@@ -60,7 +39,7 @@ def task_2d_answers():
     r = 0.04  # Risk-free rate
     delta = 0.02  # Dividend yield
     sigma = 0.2  # volatility
-    n = 3  # Number of binomial steps
+    n = 500  # Number of binomial steps
     H = 150  # Barrier
 
     call_option_price = european_binomial_up_out(S_0, K, T, r, n, H, delta, sigma)
