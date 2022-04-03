d5034a987d26a2fb8b997cbae8d94004719bd13f
Andreas Engebretsen <andreas.engebrets1@gmail.com>
2022-04-03 19:58:32 +0200

    add

--- task_3_main.py
+++ task_3_main.py
@@ -2,6 +2,6 @@ from Task_3.task_3a import task_3a_answers
 from Task_3.task_3b_c_d import task_3b_answers, task_3c_answers, task_3d_answers
 
 # task_3a_answers()
-# task_3b_answers()
+task_3b_answers()
 # task_3c_answers()
 # task_3d_answers()
