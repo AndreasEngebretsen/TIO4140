35951a4f660df13d0a6650a066d1ee81ce7d97e4
Andreas Engebretsen <andreas.engebrets1@gmail.com>
2022-04-03 18:58:06 +0200

    add

--- Task_2/task_2d.py
+++ Task_2/task_2d.py
@@ -2,7 +2,7 @@ import numpy as np
 from math_functions import nCr
 
 
-def european_binomial_up_out(s_o, k, T, r, n, B, delta, sigma):
+def european_binomial_up_out(S_0, k, T, r, n, B, delta, sigma):
     # Length of a period
     h = T / n
 
@@ -22,30 +22,29 @@ def european_binomial_up_out(s_o, k, T, r, n, B, delta, sigma):
         prob = np.power(pu, n - i) * np.power(pd, i) * nCr(n, i)
 
         # Value of stock at time t
-        s_t = s_o * np.power(u, n - i) * np.power(d, i)
+        S_T = S_0 * np.power(u, n - i) * np.power(d, i)
 
         # Intrinsic value of call at i down moves
-        int_val = np.max([s_t - k, 0])
+        int_val = np.max([S_T - k, 0])
+
+        val = int_val * prob
 
         # If the ending value of the stock, s_t,
         # is above the barrier the intrinsic value of the knock-out option is zero
-        if s_t > B:
-            int_val = 0
+        if S_T >= B:
+            p = 0
 
         # If the ending value of the stock, s_t, is below the barrier H
         # we have to correct for the probability of hitting it earlier
-        if s_t < B:
+        else:
             # Probability of hitting the barrier before ending up at final value s_t < H
-            prob_barrier = np.exp(- 2 / (np.power(sigma, 2) * T) * abs(np.log(s_o / B) * np.log(s_t / B)))
+            prob_barrier = np.exp(- 2 / (np.power(sigma, 2) * T) * abs(np.log(S_0 / B) * np.log(S_T / B)))
 
             # Probability of not hitting the barrier, which would mean that the option stays alive and has value
-            prob_alive = 1 - prob_barrier
-
-            # If s_t - K > 0, but s_t < H, we multiply with the probability of not hitting the barrier
-            int_val = int_val * prob_alive
+            p = 1 - prob_barrier
 
         # Summing up after multiplying probability and intrinsic value
-        node_sum += int_val * prob
+        node_sum += val * p
 
     # Discount the sum
     option_price = np.exp(- r * T) * node_sum
